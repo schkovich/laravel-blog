048ae5b26aa02c33c1900094a2c239d3647880f6
Goran Miskovic <schkovich@gmail.com>
2015-06-14 20:29:37 +0200

    Created remaining seeders.

---
+++ database/seeds/BlogCategoryTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BlogCategoryTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
