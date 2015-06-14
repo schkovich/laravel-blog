9a05a8f12fecee6971c21cb1da5051da27c09bbf
Goran Miskovic <schkovich@gmail.com>
2015-06-14 23:58:48 +0200

    Calling seeders for Blog and BlogCategory models.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         $this->call('BloggerTableSeeder');
+        $this->call('BlogTableSeeder');
+        $this->call('BlogCategoryTableSeeder');
 
         Model::reguard();
     }
